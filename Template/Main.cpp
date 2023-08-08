d03209baddbc3affb4b9ea7dabe459213f72dd0a
vinhuchi <vinhuchi00@gmail.com>
2023-08-08 09:46:39 +0700

    a

--- Template/Main.cpp
+++ Template/Main.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//Type
 using LL = long long;
+#define endl "\n"
+#define FileName (std::string(__FILE__).substr(0, std::string(__FILE__).find_last_of(".")))
+#define IOF() freopen((FileName + ".inp").c_str(), "r", stdin); freopen((FileName + ".out").c_str(), "w", stdout);
 //Functions
 #define IO(value)  freopen(value".inp", "r", stdin);   freopen(value".out", "w", stdout);
 #define FIO() ios_base::sync_with_stdio(false); cin.tie(NULL);
@@ -11,76 +13,6 @@ using LL = long long;
 #define loop2(x, start, end) for(int x=end-1; x >= start; x--) // pass x as end minus till reach start
 #define newl() cout << endl; // cout with no space
 
-<<<<<<< HEAD
-// Thủ tục xây dựng cây phân đoạn
-void build(int id, int l, int r) {
-    // Đoạn chỉ gồm 1 phần tử, không có nút con
-    if (l == r) {
-        st[id] = a[l];
-        return;
-    }
-
-    // Gọi đệ quy để xử lý các nút con của nút id
-    int mid = l + r >> 1; // (l + r) / 2
-    build(2 * id, l, mid);
-    build(2 * id + 1, mid + 1, r);
-    
-    // Cập nhật lại giá trị min của đoạn [l, r] theo 2 nút con
-    st[id] = min(st[2 * id], st[2 * id + 1]);
-}
-
-// Thủ tục cập nhật
-void update(int id, int l, int r, int i, int val) {
-    // i nằm ngoài đoạn [l, r], ta bỏ qua nút id
-    if (l > i || r < i) return;
-    
-    // Đoạn chỉ gồm 1 phần tử, không có nút con
-    if (l == r) {
-        st[id] = val;
-        return;
-    }
-    
-    // Gọi đệ quy để xử lý các nút con của nút id
-    int mid = l + r >> 1; // (l + r) / 2
-    update(2 * id, l, mid, i, val);
-    update(2 * id + 1, mid + 1, r, i, val);
-    
-    // Cập nhật lại giá trị min của đoạn [l, r] theo 2 nút con
-    st[id] = min(st[2 * id], st[2 * id + 1]);
-}
-
-
-// Hàm lấy giá trị
-int get(int id, int l, int r, int u, int v) {
-    // Đoạn [u, v] không giao với đoạn [l, r], ta bỏ qua đoạn này
-    if (l >  v || r <  u) return inf; 
-    
-    /* Đoạn [l, r] nằm hoàn toàn trong đoạn [u, v] mà ta đang truy vấn, 
-        ta trả lại thông tin lưu ở nút id */
-    if (l >= u && r <= v) return st[id];
-
-    // Gọi đệ quy với các nút con của nút id
-    int mid = l + r >> 1; // (l + r) / 2
-    int get1 = get(2 * id, l, mid, u, v);
-    int get2 = get(2 * id + 1, mid + 1, r, u, v);
-    
-    // Trả ra giá trị nhỏ nhất theo 2 nút con
-    return get1+ get2;
-}
-
-int main() {
-    cin >> n;
-    for (int i = 1; i <= n; ++i) cin >> a[i];
-    build(1, 1, n);
-
-    // Get q value
-    cin >> q;
-    while (q--) {
-        int type, x, y;
-        cin >> type >> x >> y;
-        if (type == 1) update(1, 1, n, x, y); // Gán giá trị y cho phần tử ở vị trí x 
-        else cout << get(1, 1, n, x, y) << '\n'; // In ra giá trị nhỏ nhất trong đoạn [x, y]
-=======
 #define read(x) cin >> x
 #define print(x) cout << x << " "; // cout with space
 #define printl(x) cout << x << endl; // cout with no space
@@ -88,25 +20,13 @@ int main() {
 
 #define Debug(x) printf("Var \"%s\" : %d\n",#x,x)
 #define DebugS(x) (cout << "Var \"" << #x << "\" : " << x << endl)
+string FileName = std::string(__FILE__).substr(0, std::string(__FILE__).find_last_of("."));
 
 //Methods
 #define I first //index
 #define V second //value
 #define PB push_back
 #define MP make_pair
-
-int main(){
-    IO("Test");
-    
-    int x;
-    read(x);
-    printl(x);
-    string b = "a";
-    DebugS(b);
-    loop2(i,0,10){
-        Debug(i);
->>>>>>> 6a41e38813c12c40ae3730c4e851380fa769fa0c
-    }
-    int a[5] ={1,2};
-    unpack(a,0,5);
+int main() {
+    return 0;
 }
